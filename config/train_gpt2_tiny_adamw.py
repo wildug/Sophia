b1939704605aa9ffb283718d3a0ddf1b06bdff16
wildug <wind1050@gmail.com>
2024-06-10 01:45:12 +0200

    added tiny_adamw config

--- config/train_gpt2_tiny_adamw.py
+++ config/train_gpt2_tiny_adamw.py
@@ -1,13 +1,13 @@
 wandb_log = True
 wandb_project = 'NanoGPT'
-wandb_run_name='gpt2-small-sophiag-10k'
+wandb_run_name='gpt2-small-adamW-10k'
 
 # these make the total batch size be ~0.5M
 # 8 batch size * 1024 block size * 6 gradaccum * 10 GPUs = 491,520
 batch_size = 8
 block_size = 1024
 gradient_accumulation_steps = 6
-total_bs = 480
+total_bs = 480 # way to large in practice
 
 n_layer = 6
 n_head = 6
@@ -26,19 +26,17 @@ log_interval = 10
 always_save_checkpoint = False  # only save best checkpoint
 
 # optimizer
-optimizer_name = 'sophiag'
+optimizer_name = 'adamw'
 learning_rate = 1e-3 # max learning rate
 weight_decay = 2e-1
-beta1 = 0.965
-beta2 = 0.99
+beta1 = 0.9
+beta2 = 0.95
 grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 # learning rate decay settings
 decay_lr = True # whether to decay the learning rate
 warmup_iters = 200 # how many steps to warm up for
 min_lr = 1.5e-5 
-rho = 0.05
-interval = 10
 
 compile = False
 
-out_dir = 'out_small_sophiag_100k'
+out_dir = 'out_small_adam_10k'
