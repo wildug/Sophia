3a3c8d3ecd8953dc9bb5c62dd5c97fc2bfa6f3a2
wildug <wind1050@gmail.com>
2024-06-10 21:19:11 +0200

    try adam with (high) max_lr 5e-2 and min_lr =0

--- config/train_gpt2_tiny_adamw.py
+++ config/train_gpt2_tiny_adamw.py
@@ -27,15 +27,15 @@ always_save_checkpoint = False  # only save best checkpoint
 
 # optimizer
 optimizer_name = 'adamw'
-learning_rate = 1e-3 # max learning rate
-weight_decay = 2e-1
+learning_rate = 5e-2 # max learning rate
+weight_decay = 3e-1
 beta1 = 0.9
 beta2 = 0.95
 grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 # learning rate decay settings
 decay_lr = True # whether to decay the learning rate
 warmup_iters = 200 # how many steps to warm up for
-min_lr = 1.5e-5 
+min_lr = 0
 
 compile = False
 
